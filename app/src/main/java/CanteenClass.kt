572a64aba8340bcde327521ffd6a890dd1c92fe2
ArslanAK47 <60480680+ArslanAK47@users.noreply.github.com>
2020-06-13 18:55:39 +0500

    class canteen added

---
+++ app/src/main/java/CanteenClass.kt
@@ -0,0 +1,6 @@
+class CanteenClass {
+    var Management: String? = "Canteen Management"
+    var Handler: String? = "Taha Hasnat"
+    var Mobile: String? = "030045334"
+    var workers: String? = "a lot of workers"
+}
