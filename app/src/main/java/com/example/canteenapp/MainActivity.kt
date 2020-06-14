dc510d0fa3a37639a4fd5da48091446eb33f8546
ArslanAK47 <60480680+ArslanAK47@users.noreply.github.com>
2020-06-14 18:25:01 +0500

    homepage updated3

--- app/src/main/java/com/example/canteenapp/MainActivity.kt
+++ app/src/main/java/com/example/canteenapp/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.canteenapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ButtonAdmin = findViewById<Button>(R.id.Adminbt)
+        ButtonAdmin.setOnClickListener {
+            val intent = Intent(this, LoginRealAdmin::class.java)
+            startActivity(intent)
+        }
     }
 }
